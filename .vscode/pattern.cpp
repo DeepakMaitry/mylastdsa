465e1b22b14609e68640e355ca0f18ccdbf96f50
Deepak Maitry <deepakmaitry9999@gmail.com>
2025-12-14 14:51:24 +0530

    solved some difficult pattern 20

--- .vscode/pattern.cpp
+++ .vscode/pattern.cpp
@@ -461,7 +461,62 @@ E E E E E
     }
     */
 
+    // pattern 20:
+
+    /*
     
+    a
+   aba 
+  abcba
+ abcdcba
+abcdedcba
+*/ 
+
+    /*int n = 5;
+    for(int i = 1; i <= n; i++) {
+        // print spaces
+        for(int j = i; j < n; j++) {
+            cout << " ";
+        }
+        // print increasing part
+        for(char ch = 'a'; ch < 'a' + i; ch++) {
+            cout << ch;
+        }
+        // print decreasing part
+        for(char ch = 'a' + i - 2; ch >= 'a'; ch--) {   // this is very important ch = 'a' + i - 2; ch>= 'a';ch--
+            cout << ch;
+        }
+        cout << endl;
+    }*/
+
+    // or using breakpoint 
+
+    // pattern 21:        // this is more important than previous one 
+    /*
+    int n = 5;
+    for(int i = 0; i <n;i++){
+        // print spaces 
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        char ch = 'A';
+        int breakpoint = (2*i + 1)/2;
+        for(int j = 0; j < 2*i+1; j++){
+            cout << ch;
+            if(j < breakpoint){
+                ch++;
+            }
+            else{
+                ch--;
+            }
+        }
+
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+*/
 
     return 0;
 }
